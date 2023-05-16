def9acb7b6235c458fc0c62e2c2023125029630d
IKERLAN\arodriguez <arodriguez@ikerlan.es>
2023-05-16 07:46:53 +0200

    bug introduced in randomnumber

--- source/randomnumber.m
+++ source/randomnumber.m
@@ -1,4 +1,4 @@
 function result = randomnumber(size1, size2, minvalue, maxvalue)
 %UNTITLED Generate a random number
-result = minvalue + (maxvalue - minvalue)*rand(size1, size2);
+result = minvalue + (maxvalue - minvalue)*rand(size1, size2)+20;
 end
